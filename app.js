310a36414dc650ba14529b3706618dbfa6d82fb9
mahnoor-shakil <mah.realone@gmail.com>
2024-02-23 00:36:55 +0500

    add one html and one css file

---
+++ app.js
@@ -0,0 +1,98 @@
+chapter 01
+
+q1
+alert ("hello world")
+
+q2
+alert ("Error!!please enter a valid password")
+
+q3
+alert ("welcome to JS Land... \n Happy Coding")
+
+q4
+alert ("welcome to JS Land...")
+
+q5
+alert ("happy coding \n Prevent this page from creating additional dialogs")
+
+q5
+alert ("hello... i can run JS through my web browser's console")
+
+q7
+C 
+
+
+CHAPTER 02
+
+q1
+var username = "mahnoor";
+
+q2
+var myname = "mahnoor shakil";
+
+q3
+var titled = "hello world";
+
+q4
+var name = "jhone Doe";
+var age = "15 year old";
+var designation = "certified mobile application development";
+alert ("name" + "age" + "designation")
+
+q5
+alert ("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+q6
+var email = "mah.realone@gmail.com";
+alert ("my email address is" + email)
+
+q7
+var book = "A smarter way to learn from the Java Script";
+alert ("i am trying to learn from the book"+book)
+
+q8
+Document.write(yah! I can write HTML Content through Javascript)
+
+chapter 03
+
+q1
+var age = "23"
+alert ("age")
+
+q3
+var birthyear = "2000"
+alert ("my birthday year is"+birthyear)
+
+q4
+var visitor's name = "john Doe"
+var product title = "shirts"
+var quantity = "5"
+alert (visitorsname+ "ordered" + quantity + "product title" + "on xyz clothing store")
+
+
+Assignment 1 
+ chapter 01
+ q1
+ alert ("firstname")
+ alert ("lastname")
+ alert ("email")
+ alert ("phone number")
+ alert ("password")
+
+ q2
+ alert ("you're learning javascript")
+
+ q3
+ alert ("hello world")
+
+chapter 02
+q1 
+var name = "MahnoorShakil";
+
+
+
+q3
+var TeamName = "Eagle";
+alert ("TeamName")
+
+
